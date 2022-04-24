ba2f11fffe5ab93854376b5d9bf2812822d0bba8
isabelamedeiros <smedeirosisabela@gmail.com>
2022-04-23 21:23:40 -0300

    Lendo os dados do json

--- src/main/java/com/api/controlevendas/controllers/ControleVendasController.java
+++ src/main/java/com/api/controlevendas/controllers/ControleVendasController.java
@@ -1,18 +1,29 @@
 package com.api.controlevendas.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/")
 public class ControleVendasController {
 
+    public static final String URL_CLIENTES = "http://www.mocky.io/v2/598b16291100004705515ec5";
+    public static final String URL_COMPRAS = "http://www.mocky.io/v2/598b16861100004905515ec7";
+
     @GetMapping("urlclientes")
     public void consultaUrlClientes() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<Object> response = restTemplate.getForObject(URL_CLIENTES, List.class);
+        System.out.println(response);
     }
 
     @GetMapping("urlcompras")
     public void consultaUrlCompras(){
+        RestTemplate restTemplate = new RestTemplate();
+        List<Object> response = restTemplate.getForObject(URL_COMPRAS, List.class);
+        System.out.println(response);
     }
 
     @GetMapping("compras")
